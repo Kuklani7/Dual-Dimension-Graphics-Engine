677c8a44525bd642e4f6660d207a1be381dbd014
Kuklani7 <kuklanirt@gmail.com>
2026-01-20 00:14:10 -0500

    Delete CSE116-GameEngine directory

--- CSE116-GameEngine/src/main/java/app/games/topdownobjects/PlayerMagicProjectile.java
+++
@@ -1,9 +0,0 @@
-package app.games.topdownobjects;
-
-import app.gameengine.model.physics.Vector2D;
-
-public class PlayerMagicProjectile extends Projectile{
-    public PlayerMagicProjectile(Vector2D location){
-        super(location,3);
-    }
-}
